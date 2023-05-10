3d516b1717f4df10c21bef7911851e8f87cfeed5
Vladyslav Shostak <113975751+VlShost@users.noreply.github.com>
2023-05-10 14:43:25 +0300

    import hw2

---
+++ src/components/ContactForm/ContactFrom.jsx
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import css from './ContactForm.module.css';
+
+export default class ContactForm extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  handleInputChange = e => {
+    const { name, value } = e.currentTarget;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.onSubmit(this.state);
+    this.setState({ name: '', number: '' });
+  };
+
+  render() {
+    const { handleSubmit, state, handleInputChange } = this;
+    return (
+      <form className={css.contactForm} onSubmit={handleSubmit}>
+        <label className={css.contactLabel}>
+          Name
+          <input
+            className={css.contactInput}
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+            value={state.name}
+            onChange={handleInputChange}
+          />
+        </label>
+        <label className={css.contactLabel}>
+          Number
+          <input
+            className={css.contactInput}
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+            value={state.number}
+            onChange={handleInputChange}
+          />
+        </label>
+        <button className={css.contactBtn} type="submit">
+          Add contact
+        </button>
+      </form>
+    );
+  }
+}
+
+ContactForm.propTypes = {
+  name: PropTypes.string,
+  number: PropTypes.number,
+};
